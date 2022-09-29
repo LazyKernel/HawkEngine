a5bdf07ccb0794419b80386dde77bc88162562ca
LazyKernel <miksu.fredriksson@hotmail.com>
2022-09-30 00:30:07 +0300

    Add instance creation

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,48 @@
     clippy::cargo
 )]
 
-use anyhow::Result;
+use std::sync::Arc;
+use anyhow::{anyhow, Result};
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
+use vulkano::VulkanLibrary;
+use vulkano::instance::{
+    Instance, 
+    InstanceCreateInfo,
+    Version
+};
 
 #[derive(Clone, Debug)]
-struct App;
+struct App {
+    instance: Arc<Instance>
+}
 
 impl App {
     unsafe fn create(_window: &Window) -> Result<Self> {
-        Ok(Self {})
+        let instance = App::create_instance()?;
+        Ok(Self { instance })
+    }
+
+    unsafe fn create_instance() -> Result<Arc<Instance>> {
+        let library = VulkanLibrary::new().unwrap();
+        let required_extensions = vulkano_win::required_extensions(&library);
+
+        let create_info = InstanceCreateInfo {
+            application_name: Some("Hawk Engine - Test".into()),
+            application_version: Version { major: 0, minor: 0, patch: 1 },
+            engine_name: Some("Hawk Engine".into()),
+            engine_version: Version { major: 0, minor: 0, patch: 1 },
+            enabled_extensions: required_extensions,
+            enumerate_portability: true,
+            ..Default::default()
+        };
+
+        let res = Instance::new(library, create_info)
+            .map_err(|b| anyhow!("{}", b))?;
+
+        return Ok(res);
     }
 
     unsafe fn render(&mut self, _window: &Window) -> Result<()> {
