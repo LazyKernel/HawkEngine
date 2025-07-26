6c2090f24be3c0c5ad5c97bee6ba3287c3b972c6
LazyKernel <miksu.fredriksson@hotmail.com>
2025-07-27 00:08:14 +0300

    Sending and receiving of serialized packets for server and client

--- src/network/main.rs
+++ src/network/main.rs
@@ -1,25 +1,32 @@
-use std::{collections::HashMap, env, error::Error, net::SocketAddr, time::Instant};
+use std::{collections::HashMap, env, error::Error, net::SocketAddr, sync::Arc, time::Instant};
 use log::{error, info, log, warn};
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{tcp, TcpListener, TcpStream, UdpSocket}};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{tcp, TcpListener, TcpStream, UdpSocket}, sync::{broadcast, mpsc}};
 use uuid::{uuid, Uuid};
 use serde::{Serialize, Deserialize};
 
 struct Client {
     client_id: Uuid,
+    addr: SocketAddr,
     last_keep_alive: Instant
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 enum NetworkMessageType {
     Unknown = 0,
     ConnectionRequest,
     ConnectionAccept,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct NetworkMessage {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct NetworkMessagePacket {
     message_type: NetworkMessageType,
     payload: Vec<u8>
+}
+
+#[derive(Clone, Debug)]
+struct NetworkMessage {
+    addr: SocketAddr,
+    packet: NetworkMessagePacket
 }
 
 #[derive(Serialize, Deserialize)]
@@ -28,29 +35,28 @@ struct ConnectionAccepted {
     server_version: String,
 }
 
-impl Default for Client {
-    fn default() -> Self {
+impl Client {
+    fn new(addr: SocketAddr) -> Self {
         Self {
             client_id: Uuid::new_v4(),
+            addr: addr,
             last_keep_alive: Instant::now()
         }
     }
 }
 
-fn build_network_message<T: Serialize>(message_type: NetworkMessageType, payload: Option<T>) -> Result<Vec<u8>, rmp_serde::encode::Error> {
-    let net_msg = NetworkMessage {
+fn build_network_message<T: Serialize>(message_type: NetworkMessageType, payload: Option<T>) -> Result<NetworkMessagePacket, rmp_serde::encode::Error> {
+    Ok(NetworkMessagePacket {
         message_type: message_type,
         payload: match payload {
             Some(v) => rmp_serde::to_vec(&v)?,
             None => Vec::<u8>::default(),
         }
-    };
-
-    return rmp_serde::to_vec(&net_msg);
+    })
 }
 
-fn server_handle_connect(clients: &mut HashMap<SocketAddr, Client>, addr: SocketAddr) -> Vec<u8> {
-    let client = Client::default();
+fn server_handle_connect(clients: &mut HashMap<SocketAddr, Client>, addr: SocketAddr) -> NetworkMessagePacket {
+    let client = Client::new(addr);
 
     println!("New client connected with ID: {}", client.client_id);
 
@@ -61,9 +67,6 @@ fn server_handle_connect(clients: &mut HashMap<SocketAddr, Client>, addr: Socket
     return conn_acc_msg;
 }
 
-fn client_handle_connect() {
-
-}
 
 async fn server() {
     let tcp_listener = TcpListener::bind("127.0.0.1:6782").await.unwrap();
@@ -71,52 +74,169 @@ async fn server() {
 
     let mut clients: HashMap<SocketAddr, Client> = Default::default();
 
-    loop {
-        let (mut socket, addr) = tcp_listener.accept().await.unwrap();
+    let (a2s_sender, mut a2s_receiver) = mpsc::channel::<NetworkMessage>(16384);
+    let (s2a_sender, s2a_receiver) = broadcast::channel::<NetworkMessage>(16384);
 
-        tokio::spawn(async move {
-            let mut buf = [0u8; 512];
-            if let Ok(num_bytes) = socket.read(&mut buf[..]).await {
-                println!("Read n bytes: {:?}", num_bytes);
-                match rmp_serde::from_slice::<NetworkMessage>(&buf[..num_bytes]) {
-                    Ok(v) => {
-                        match v.message_type {
-                            NetworkMessageType::ConnectionRequest => {
-                                let conn_acc_msg = server_handle_connect(&mut clients, addr);
-                                let _ = socket.write_all(conn_acc_msg.as_slice()).await;
+
+    tokio::spawn(async move {
+        loop {
+            let (socket, addr) = tcp_listener.accept().await.unwrap();
+
+            let (mut rx_socket, mut tx_socket) = socket.into_split();
+            let sender = a2s_sender.clone();
+
+            // receiving from this client
+            tokio::spawn(async move {
+                let mut buf = [0u8; 512];
+
+                loop {
+                    if let Ok(num_bytes) = rx_socket.read(&mut buf[..]).await {
+                        println!("Read n bytes: {:?}", num_bytes);
+                        match rmp_serde::from_slice::<NetworkMessagePacket>(&buf[..num_bytes]) {
+                            Ok(v) => {
+                                if let Err(e) = sender.send(NetworkMessage { addr: addr, packet: v }).await {
+                                    error!("Error occurred while trying to pass packet from task, the queue might be full: {:?}", e);
+                                }
                             },
-                            _ => warn!("Unsupported message type: {:?}", v.message_type),
+                            Err(e) => error!("Error parsing received buffer: {:?}", e),
+                        }
+                    }
+                }
+            });
+
+            // sending to this client
+            let mut rx = s2a_receiver.resubscribe();
+            tokio::spawn(async move {
+
+                loop {
+                    if let Ok(data) = rx.recv().await {
+                        if data.addr == addr {
+                            // this is for us
+                            match rmp_serde::to_vec(&data.packet) {
+                                Ok(v) => {
+                                    if let Err(e) = tx_socket.write_all(v.as_slice()).await {
+                                        error!("Could not write to socket: {:?}", e);
+                                    }
+                                },
+                                Err(e) => error!("Could not serialize data: {:?}", e),
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    });
+
+    // NOTE: this would be run once per frame in the update loop
+    loop {
+
+        // collect all messages, up to a cap so we can't stall
+        let mut n_recv = 0;
+        while !a2s_receiver.is_empty() && n_recv < 10000 {
+            if let Ok(data) = a2s_receiver.try_recv() {
+                match data.packet.message_type {
+                    NetworkMessageType::ConnectionRequest => {
+                        let conn_acc_msg = server_handle_connect(&mut clients, data.addr);
+                        if let Err(e) = s2a_sender.send(NetworkMessage { addr: data.addr, packet: conn_acc_msg }) {
+                            error!("Could not send message to broadcast queue, might be full: {:?}", e);
+                        }
+                    },
+                    _ => warn!("Unsupported message type: {:?}", data.packet.message_type),
+                }
+            }
+
+            n_recv += 1;
+        }
+    }
+}
+
+
+fn client_handle_connect(local_addr: SocketAddr, packet: &NetworkMessagePacket) -> Result<Client, rmp_serde::decode::Error> {
+    let accept_data = rmp_serde::from_slice::<ConnectionAccepted>(&packet.payload)?;
+    Ok(Client { addr: local_addr, client_id: accept_data.client_id, last_keep_alive: Instant::now() })
+}
+
+async fn client() {
+    let tcp_stream = TcpStream::connect("127.0.0.1:6782").await.expect("Could not connect to server");
+    //let mut udp_stream = UdpSocket::bind("0.0.0.0:6782").await.expect("Could not connect to server over UDP");
+
+    
+    let mut client: Option<Client> = None;
+
+    let (a2s_sender, mut a2s_receiver) = mpsc::channel::<NetworkMessage>(16384);
+    let (s2a_sender, mut s2a_receiver) = mpsc::channel::<NetworkMessage>(16384);
+
+    let local_addr = tcp_stream.local_addr().unwrap();
+    let peer_addr = tcp_stream.peer_addr().unwrap();
+    let (mut rx_socket, mut tx_socket) = tcp_stream.into_split();
+
+    // receiving from server
+    tokio::spawn(async move {
+        let mut buf = [0u8; 512];
+
+        loop {
+            if let Ok(num_bytes) = rx_socket.read(&mut buf[..]).await {
+                println!("Read n bytes: {:?}", num_bytes);
+                match rmp_serde::from_slice::<NetworkMessagePacket>(&buf[..num_bytes]) {
+                    Ok(v) => {
+                        if let Err(e) = a2s_sender.send(NetworkMessage { addr: rx_socket.peer_addr().unwrap(), packet: v }).await {
+                            error!("Error occurred while trying to pass packet from task, the queue might be full: {:?}", e);
                         }
                     },
                     Err(e) => error!("Error parsing received buffer: {:?}", e),
                 }
             }
-        });
-    }
-}
+        }
+    });
 
-async fn client() {
-    let mut tcp_stream = TcpStream::connect("127.0.0.1:6782").await.expect("Could not connect to server");
-    //let mut udp_stream = UdpSocket::bind("0.0.0.0:6782").await.expect("Could not connect to server over UDP");
+    // sending to this client
+    tokio::spawn(async move {
 
-    let _ = tcp_stream.write_all(b"connect").await;
-    
-    let mut client: Option<Client> = None;
-
-    loop {
-        let mut buf = [0u8; 512];
-        if let Ok(num_bytes) = tcp_stream.read(&mut buf[..]).await {
-
-            if num_bytes > 0 {
-                println!("Read n bytes: {:?}", num_bytes);
-                match rmp_serde::from_slice::<ConnectionAccepted>(&buf[..]) {
+        loop {
+            if let Some(data) = s2a_receiver.recv().await {
+                match rmp_serde::to_vec(&data.packet) {
                     Ok(v) => {
-                        client = Some(Client { client_id: v.client_id, last_keep_alive: Instant::now() });
-                        println!("Connected with ID: {}", v.client_id);
+                        if let Err(e) = tx_socket.write_all(v.as_slice()).await {
+                            error!("Could not write to socket: {:?}", e);
+                        }
                     },
-                    Err(e) => error!("Could not deserialize:\n{}", e),
+                    Err(e) => error!("Could not serialize data: {:?}", e),
                 }
             }
+            else {
+                // the channel has been closed, exit
+                break;
+            }
+        }
+    });
+
+
+    let msg = NetworkMessagePacket {message_type: NetworkMessageType::ConnectionRequest, payload: vec![]};
+    let _ = s2a_sender.send(NetworkMessage { addr: peer_addr, packet: msg });
+
+
+    // NOTE: this would be run once per frame in the update loop
+    loop {
+
+        // collect all messages, up to a cap so we can't stall
+        let mut n_recv = 0;
+        while !a2s_receiver.is_empty() && n_recv < 10000 {
+            if let Ok(data) = a2s_receiver.try_recv() {
+                match data.packet.message_type {
+                    NetworkMessageType::ConnectionAccept => {
+                        match client_handle_connect(local_addr, &data.packet) {
+                            Ok(v) => {
+                                client = Some(v);
+                                println!("Connected with ID: {}", client.unwrap().client_id);
+                            },
+                            Err(e) => error!("Failed to parse ConnectionAccept payload: {:?}", e),
+                        }
+                    },
+                    _ => warn!("Unsupported message type: {:?}", data.packet.message_type),
+                }
+            }
+
+            n_recv += 1;
         }
     }
 }
@@ -124,6 +244,8 @@ async fn client() {
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
+
     let args: Vec<String> = env::args().collect();
 
     if args.contains(&"--server".to_string()) || args.contains(&"-s".to_string()) {
